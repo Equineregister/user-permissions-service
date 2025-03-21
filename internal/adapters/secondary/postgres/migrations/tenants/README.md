164b527835c0aa6a446c945366ec006ab7c28f79
Mark Songhurst <MarkSonghurst@users.noreply.github.com>
2025-03-21 16:20:28 +0300

    Added tenant_id for WG data pop

--- internal/adapters/secondary/postgres/migrations/tenants/README.md
+++ internal/adapters/secondary/postgres/migrations/tenants/README.md
@@ -1,9 +1,12 @@
 # user_permissions_service - Tenant Specific Data Population
 
 
-This directory contains a data population SQL script for each individual Tenant.
+This directory contains a data population SQL script for each Tenant.
 A script must exist for each Tenant. If one Tenant has the exact same data population requirements of another Tenant then copy the script - don't share it!
 
 The data population scripts are intended as a foundation for the contents of the Tenant's database, regular API use will continue population of the database.
 
 No consideration has been made for revert scripts.
+
+For Tenant IDs, refer to https://scanimal.atlassian.net/wiki/spaces/WG/pages/1590820882/Tenant+IDs
+
--- internal/adapters/secondary/postgres/migrations/tenants/westgen/0001-foundation.sql
+++ internal/adapters/secondary/postgres/migrations/tenants/westgen/0001-foundation.sql
@@ -1,8 +1,8 @@
-INSERT INTO roles (role_id, role_name) VALUES
-    ('064a039c-2b34-4598-a2e7-2dc3fe09d24c', 'administrator'),
-    ('4c1cd50c-7eaa-422c-9466-deed96e9bfa5', 'warehouse staff'),
-    ('1878e631-80af-4707-98ff-24029adee25d', 'sales representative');
+INSERT INTO roles (tenant_id, role_id, role_name) VALUES
+    ('a2187e39-e9f0-4f83-b17c-56c04d6198cb','064a039c-2b34-4598-a2e7-2dc3fe09d24c', 'administrator'),
+    ('a2187e39-e9f0-4f83-b17c-56c04d6198cb','4c1cd50c-7eaa-422c-9466-deed96e9bfa5', 'warehouse staff'),
+    ('a2187e39-e9f0-4f83-b17c-56c04d6198cb','1878e631-80af-4707-98ff-24029adee25d', 'sales representative');
 
-    INSERT INTO resource_types (resource_type_id, resource_type_name) VALUES
-    (1, 'customers'),
-    (2, 'contacts');
+    INSERT INTO resource_types (tenant_id, resource_type_id, resource_type_name) VALUES
+    ('a2187e39-e9f0-4f83-b17c-56c04d6198cb', 1, 'customers'),
+    ('a2187e39-e9f0-4f83-b17c-56c04d6198cb', 2, 'contacts');
